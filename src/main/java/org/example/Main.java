8b535f7eb8d4544d7d514930416dd1bfde0bdb5d
gorkemiskeceli <mgiskec@gmail.com>
2025-02-27 16:54:50 +0300

    welcome message has been added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,12 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+
+        System.out.println("--WELCOME TO GORKEM BANK--");
 
     }
 }
